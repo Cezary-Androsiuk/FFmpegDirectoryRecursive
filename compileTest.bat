55bcd5ec24cbc4555372250f1f1723d9fa7e83e1
Cezary Androsiuk PC <czandros@wp.pl>
2024-09-08 16:45:20 +0200

    moved executable to bin/ and changed compile commands

--- compileTest.bat
+++ compileTest.bat
@@ -1,1 +1,1 @@
-g++ main.cpp cpp\MainMethods.cpp cpp\FFExecute.cpp cpp\FFTester.cpp cpp\ListMaker.cpp cpp\OtherError.cpp cpp\ChangeFileDate.cpp cpp\TestPipe.cpp cpp\WinConsoleHandler.cpp cpp\HandlePipeOutput.cpp -o ffmpegRecTest.exe
+g++ main.cpp cpp\MainMethods.cpp cpp\FFExecute.cpp cpp\FFTester.cpp cpp\ListMaker.cpp cpp\OtherError.cpp cpp\ChangeFileDate.cpp cpp\TestPipe.cpp cpp\WinConsoleHandler.cpp cpp\HandlePipeOutput.cpp -o bin\ffmpegRecTest.exe
